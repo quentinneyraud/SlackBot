54bea92ecd3aa1b9df39c86d2d01b27acbc99eed
Quentin Neyraud <quentin.neyraud@gmail.com>
2015-10-21 00:11:15 +0200

    add contact requests

--- Requests/contactRequest.js
+++ Requests/contactRequest.js
@@ -2,16 +2,40 @@ var contactRequest = function Constructor(misterBot){
     this.models = misterBot.models;
     this.message = misterBot.message;
     this.misterBot = misterBot;
-    this._getAll();
+    this.request = {
+        'userName' : "",
+        'field' : ""
+    }
+
+    this._setRequestInfos();
+    this._selectUser();
 }
 
+contactRequest.prototype._answerContact = function(users){
+    if(users.length == 0){
+        this.misterBot.answer("Tu m'as appelé mais j'ai pas trouvé cette personne");
+    }else{
+        if(!users[0][this.request.field]){
+            this.misterBot.answer("Désolé mais je ne connais pas le " + this.request.field + " de " + this.request.userName);
+        }else{
+            this.misterBot.answer("Voici le " + this.request.field + " de " + this.request.userName + " : *" + users[0][this.request.field] + "*");
+        }
+    }
+}
 
-contactRequest.prototype._getAll = function(){
+contactRequest.prototype._selectUser = function(){
     var self = this;
-    this.models.userModel.find({}, function(err, users) {
+    this.models.userModel.find({
+        'prenom' : self.request.userName
+    }, function(err, users) {
         if (err) throw err;
-        self.misterBot._respond("Voila votre réponse : " + users[0].numero);
+        self._answerContact(users);
     });
+}
+
+contactRequest.prototype._setRequestInfos = function(){
+    this.request.field = (this.message.text.toLowerCase().indexOf('numero') > -1 || this.message.text.toLowerCase().indexOf('numéro') > -1) ? "numero" : "mail";
+    this.request.userName = this.message.text.split(' ').pop().toLowerCase();
 }
 
 module.exports = contactRequest;
