963a16924a85e74f59ba757cf8de614736ff7cb8
Quentin Neyraud <quentin.neyraud@gmail.com>
2015-10-21 00:09:06 +0200

    update man infos

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 var config = [
     {
         requestTitle : "contactRequest",
-        regex : /numero de \w+$/,
-        stringCatch : "(numero|numéro) de < Prenom >",
-        description : "Retourne le numero de la personne"
+        regex : /(numero|numéro) de \w+$/,
+        stringCatch : "numero de < Prenom >",
+        description : "Donné le numéro d'une personne"
     },
     {
         requestTitle : "contactRequest",
         regex : /mail de \w+$/,
         stringCatch : "mail de < Prenom >",
-        description : "Retourne le mail de la personne"
+        description : "Donné le mail d'une personne"
     },
     {
         requestTitle : "weatherRequest",
         regex : /(meteo|météo) (du \d{2}\/\d{2}|du \d{2}\/\d{2}\/\d{4}|de demain|d'aujourd'hui)$/,
-        stringCatch : "meteo < d'aujourd'hui / de demain / du jj/mm / du jj/mm/yyyy >",
-        description : "Retourne la météo du jour demandé"
+        stringCatch : "meteo < d'aujourd'hui > ou < de demain > ou < du jj/mm > ou < du jj/mm/yyyy >",
+        description : "Donné la météo"
     }
 ]
 
